03f7366985cdd2698931d2c541785a268e784fba
ShankarAmanchi2007 <aamanchi.shankar2007@gmail.com>
2026-01-04 20:13:01 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,31 +1,52 @@
 
+export interface Project {
+  id: string;
+  title: string;
+  description: string;
+  fullDescription: string;
+  problemStatement?: string;
+  solutionStatement?: string;
+  image: string;
+  techStack: string[];
+  isPaid: boolean;
+  price?: number;
+  demoUrl: string;
+  authorId: string;
+  authorName: string;
+  authorAvatar: string;
+  views: number;
+}
+
+export interface Achievement {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+export interface Reward {
+  name: string;
+  year: string;
+  platform: string;
+}
+
 export interface Developer {
   id: string;
   name: string;
   avatar: string;
   skills: string[];
   bio: string;
-  githubUrl: string;
-  projectsCount: number;
-}
-
-export interface Project {
-  id: string;
-  title: string;
-  shortDescription: string;
-  longDescription: string;
-  image: string;
-  techStack: string[];
-  type: 'Free' | 'Paid';
-  price?: string;
-  developerId: string;
-  demoUrl: string;
-  likes: number;
+  github: string;
+  contactEmail: string;
+  projects: Project[];
+  achievements: Achievement[];
+  rewards: Reward[];
+  rank?: string;
 }
 
 export interface User {
   id: string;
-  email: string;
   name: string;
-  isDeveloper: boolean;
+  email: string;
+  avatar: string;
+  github?: string;
 }
